677b7b1bbdb32cc5e32b69a116d7dfbcdadd315e
Michael Reynolds <michael4reynolds@gmail.com>
2017-03-30 18:18:34 -0400

    eject and prep

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import log, {hookConsoleLog} from 'stacklogger'
+import classNames from 'classnames'
+
+// Model
+
+// View
+
+// Controller
+
+// initialize
+function init() {
+  try {
+    log('init Timer App')
+  } catch (e) {
+    log(e)
+  }
+}
+
+hookConsoleLog()
+init()
